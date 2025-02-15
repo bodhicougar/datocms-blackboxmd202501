0f6784f190bb527d9290afa8b79b884201eb7ffd
libero <info@blackboxmd.work>
2025-02-15 12:48:56 +0100

    stackbitVersion setzen

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -2,7 +2,9 @@
 import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
-  // ...
+stackbitVersion: "~0.6.0",
+  ssgName: "custom",
+  nodeVersion: "18",
   modelExtensions: [
     // Extend the "Page" and "Post" models by defining them as page models
     { name: 'Page', type: 'page' },
